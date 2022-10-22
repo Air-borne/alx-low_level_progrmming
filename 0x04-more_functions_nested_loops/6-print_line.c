3a9bf2a23f9b31bd9854e445c9cbbf901f0c1e7f
Air-borne <sakyibarnard77@gmail.com>
2022-10-22 21:32:33 +0000

    betty check

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line using the character _.
- * @n: number of characters to be printed on screen.
- */
- 
+* print_line - Draws a straight line using the character _.
+* @n: number of characters to be printed on screen.
+*/
+
 void print_line(int n)
 {
 	int com;
