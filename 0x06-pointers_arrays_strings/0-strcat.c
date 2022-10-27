687264bccbf1f86d2d1d3ed1b03fbc4c5040343e
Air-borne <sakyibarnard77@gmail.com>
2022-10-27 14:14:38 +0000

    Add task 0 to github

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strcat(char *dest, char *src): concatenates two strings
+* @dest - passes a string to the function
+* @src - passes another string to be added to the
+* previous string
+* Returns - a pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int length;
+
+	/* store the length of the destination in length*/
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		++length;
+	}
+
+	/*concatenate source to destination*/
+	for (i = 0; src[i] != '\0'; ++i, ++length)
+	{
+		dest[length] += src[i];
+	}
+
+	/*terminate the destination string*/
+	dest[length] = '\0';
+	return (dest);
+}
