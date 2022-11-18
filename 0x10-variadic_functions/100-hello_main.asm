28b3a84bc953800296727e89dedc44c58cb785c0
Air-borne <sakyibarnard77@gmail.com>
2022-11-18 04:31:10 +0000

    Do all together

---
+++ 0x10-variadic_functions/100-hello_main.asm
@@ -0,0 +1,17 @@
+SECTION .data
+msg     db      'Hello, Main', 0Ah
+ 
+SECTION .text
+global  main
+ 
+main:
+ 
+    mov     edx, 17
+    mov     ecx, msg
+    mov     ebx, 1
+    mov     eax, 4
+    int     80h
+ 
+    mov     ebx, 0
+    mov     eax, 1
+    int     80h
